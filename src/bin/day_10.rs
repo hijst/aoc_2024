2b1ce45cd61aee264010568740e1ca686032d50d
Thijs Visser <thijsvisser@bol.com>
2024-12-12 00:49:33 +0100

    day 11

--- src/bin/day_10.rs
+++ src/bin/day_10.rs
@@ -1,3 +1,80 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+    collections::HashSet,
+};
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
 fn main() {
-    println!("day 1 solution: xd");
+    use std::time::Instant;
+    let now = Instant::now();
+    solve();
+    let elapsed = now.elapsed();
+    println!("took {:.2?}", elapsed);
+}
+
+fn solve() {
+    let grid: Vec<Vec<i64>> = lines_from_file("../input/10.txt").into_iter()
+        .map(|line| line
+            .chars()
+            .map(|c| c
+                .to_string()
+                .parse::<i64>()
+                .unwrap()
+                ).collect()
+            ).collect();
+
+    let h = grid.len();
+    let w = grid[0].len();
+    let mut ans1 = 0;
+    let mut ans2 = 0;
+
+    for i in 0..h {
+        for j in 0..w {
+            if grid[i][j] == 0 {
+                let mut peaks: Vec<(usize, usize)> =  vec![];
+                //println!("score for ({},{}) = {}",i,j,neighbour_scores(i,j,&grid));
+                //ans1 += neighbour_scores(i,j,&grid)
+                peaks.append(&mut neighbour_scores(i,j,&grid));
+                ans1 +=  HashSet::<(usize, usize)>::from_iter(peaks.clone()).len();
+                ans2 += peaks.len();
+            }
+        }
+    }
+
+    println!("answer 1: {}", ans1); // 5086
+    println!("answer 2: {}", ans2); // 1770
+}
+
+fn neighbour_scores(i: usize, j: usize, grid: &Vec<Vec<i64>>) -> Vec<(usize,usize)> {
+    // base case
+    if grid[i][j] == 9 {
+        return vec![(i,j)];
+    }
+
+    let mh = grid.len() - 1;
+    let mw = grid[0].len() - 1;
+    let val = grid[i][j];
+    let mut neighbours: Vec<(usize,usize)> = vec![];
+    let mut res: Vec<(usize, usize)> = vec![];
+
+    if i > 0 { neighbours.push((i - 1, j)) }
+    if j > 0 { neighbours.push((i, j - 1)) }
+    if i < mh { neighbours.push((i + 1, j)) }
+    if j < mw { neighbours.push((i, j + 1)) }
+    for n in neighbours {
+        if grid[n.0][n.1] == val + 1 {
+            //res += neighbour_scores(n.0, n.1, grid);
+            res.append(&mut neighbour_scores(n.0, n.1, grid))
+        }
+    }
+    return res;
 }
--- src/bin/day_11.rs
+++ src/bin/day_11.rs
@@ -1,3 +1,75 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+    collections::HashMap,
+};
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
 fn main() {
-    println!("day 1 solution: xd");
+    use std::time::Instant;
+    let now = Instant::now();
+    solve();
+    let elapsed = now.elapsed();
+    println!("took {:.2?}", elapsed);
+}
+
+fn solve() {
+    let nums: Vec<i64> = lines_from_file("../input/11.txt")
+        .into_iter()
+        .flat_map(|l| { l
+            .split(" ")
+            .flat_map(|s| s
+                .parse::<i64>().ok()
+                )
+            .collect::<Vec<i64>>()
+        }).collect::<Vec<i64>>();
+
+    let mut dp: HashMap<(i64, i64), i64> = HashMap::new();
+
+    let (ans, ans2) = nums
+        .iter()
+        .fold((0,0), |(a1, a2), num| 
+            (a1 + resolve(*num, 25, &mut dp), a2 + resolve(*num, 75, &mut dp))
+            );
+
+    println!("answer 1: {}", ans); //203457 
+    println!("answer 2: {}", ans2); //241394363462435
+}
+
+fn resolve(num: i64, left: i64, dp: &mut HashMap<(i64,i64), i64>) -> i64 {
+    if left == 0 {
+        return 1;
+    }
+
+    if dp.contains_key(&(num,left)) {
+        return dp[&(num,left)];
+    }
+
+    let res = next_nums(num).into_iter().map(|n| resolve(n, left - 1, dp)).sum();
+    dp.entry((num,left)).or_insert(res);
+    return res
+}
+
+fn next_nums(num: i64) -> Vec<i64> {
+    if num == 0 {
+        return vec![1];
+    }
+    if num.to_string().len() % 2 == 0 {
+        return split_num(num.to_string());
+    }
+    return vec![num * 2024];
+}
+
+fn split_num(num: String) -> Vec<i64> {
+    let char_len = num.len();
+    let (first_part, second_part) = num.split_at(char_len / 2);
+    return vec![first_part.parse::<i64>().unwrap(), second_part.parse::<i64>().unwrap()];
 }
